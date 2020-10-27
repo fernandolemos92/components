770e90b409ce89adc11773efe9cff5e02f347e0f
Arthur de Castro <arthur_ravin@hotmail.com>
2020-10-26 22:32:32 -0300

    Create TitleAndSubtitle module

--- src/App.js
+++ src/App.js
@@ -9,11 +9,33 @@ import { Radius4BtnPrimary } from "./Components/Radius4BtnPrimary";
 import { Radius4BtnSecondary } from "./Components/Radius4BtnSecondary";
 import { InfoAffiliated } from "./Components/InfoAffiliated";
 import { PostIt } from "./Components/ClinicIdentifier/PostIt";
+import { Container, Grid } from "@material-ui/core";
+import { TitleAndSubtitle } from "./Components/ClinicIdentifier/TitleAndSubtitle";
 
 function App() {
   return (
     <>
-      <PostIt />
+      <Container
+        style={{
+          backgroundColor: "black",
+          maxWidht: "700px",
+          padding: 16,
+          marginTop: 32,
+        }}
+      >
+        <Grid spacing={4} justify="center" container>
+          <Grid item>
+            <TitleAndSubtitle
+              title={"TORNEIO"}
+              subtitle={"#1 COPA MIX TGG - CSGO"}
+            />
+          </Grid>
+          <Grid item>
+            <TitleAndSubtitle title={"DATA"} subtitle={"26/09/2020"} />
+          </Grid>
+        </Grid>
+        <PostIt />
+      </Container>
       <CssBaseline />
       {/* <InfoPanel /> */}
       {/* <MeshDetails /> */}
